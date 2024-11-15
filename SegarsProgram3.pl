dc2c746a5b7632094fc607c9585f71e945e5f844
bailee1531 <baileesegars2016@gmail.com>
2024-11-15 09:34:21 -0600

    Added functionality to count the number of times an element appears in a list. Completed part 1 of assignment

--- SegarsProgram3.pl
+++ SegarsProgram3.pl
@@ -3,3 +3,9 @@
 %% CS 424-01
 %% I did not use AI, copy from another person, or copy this solution from the internet.
 %% Used the online sandbox version of SWI-Prolog
+
+%% Part 1. Count the number of times an element appears in a list.
+countValues(_, [], 0).  %% Base case
+countValues(H, [H|T], C) :-!, countValues(H, T, C2), C is C2 + 1.   %% Recursively call countValues if the element is found in the head and the tail.
+%% ! above prevents from checking the countValues below.
+countValues(H, [_|T], C) :- countValues(H, T, C).   %% Recursively call countValues if the element is found in the tail.
