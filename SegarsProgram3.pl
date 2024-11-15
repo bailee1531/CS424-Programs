72b8d14d0f4c52bb50a1b5fd4b5647ef7d640afd
bailee1531 <baileesegars2016@gmail.com>
2024-11-15 09:22:13 -0600

    Created program file

---
+++ SegarsProgram3.pl
@@ -0,0 +1,5 @@
+%% Bailee Segars
+%% 11/15/2024
+%% CS 424-01
+%% I did not use AI, copy from another person, or copy this solution from the internet.
+%% Used the online sandbox version of SWI-Prolog
